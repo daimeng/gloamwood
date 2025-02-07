77be277e8a7be9521dcf8a17b503c87edd69c3f9
Daimeng Chen <daimengchen@gmail.com>
2025-02-07 19:13:55 +0800

    move world struct to new file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use macroquad::input;
 use macroquad::prelude::*;
 mod mapgen;
+mod worldmap;
 
 const Si: i16 = 16;
 const S: f32 = Si as f32;
@@ -10,96 +11,6 @@ type Point = (i16, i16);
 
 const BG_COLOR: Color = color_u8!(15, 15, 23, 255);
 const TERRAIN_TINT: Color = color_u8!(255, 255, 255, 150);
-
-struct WorldMap {
-    mapw: usize,
-    maph: usize,
-    terrains: Vec<Vec<i16>>,
-    monsters: Vec<Vec<i16>>,
-    auras: Vec<Vec<i16>>,
-    open: Vec<Vec<bool>>,
-    search_buffer: Vec<(usize, usize)>,
-}
-
-impl WorldMap {
-    fn new(mapw: usize, maph: usize) -> Self {
-        Self {
-            mapw,
-            maph,
-            terrains: vec![vec![0; mapw]; maph],
-            monsters: vec![vec![0; mapw]; maph],
-            auras: vec![vec![0; mapw]; maph],
-            open: vec![vec![false; mapw]; maph],
-            search_buffer: vec![(0, 0); maph * mapw],
-        }
-    }
-
-    fn set_monster(&mut self, x: usize, y: usize, n: i16) {
-        self.monsters[y][x] = n;
-        for yi in 0..3 {
-            let yy = y + yi;
-            if yy == 0 || yy > self.maph {
-                continue;
-            }
-
-            for xi in 0..3 {
-                let xx = x + xi;
-                if xx == 0 || xx > self.mapw {
-                    continue;
-                }
-
-                self.auras[yy - 1][xx - 1] += n;
-            }
-        }
-    }
-
-    fn open_tile(&mut self, x: usize, y: usize, val: Option<bool>) -> bool {
-        // clamp x y
-        let x = if x >= self.mapw { self.mapw - 1 } else { x };
-        let y = if y >= self.maph { self.maph - 1 } else { y };
-
-        if let Some(v) = val {
-            let mut j = 0;
-
-            self.search_buffer[j] = (x, y);
-            j += 1;
-
-            while j > 0 {
-                let (xx, yy) = self.search_buffer[j - 1];
-                j -= 1;
-
-                if self.open[yy][xx] {
-                    continue;
-                };
-
-                self.open[yy][xx] = true;
-
-                if self.auras[yy][xx] > 0 {
-                    continue;
-                }
-
-                if yy < self.maph - 1 && !self.open[yy + 1][xx] {
-                    self.search_buffer[j] = (xx, yy + 1);
-                    j += 1;
-                }
-                if xx < self.mapw - 1 && !self.open[yy][xx + 1] {
-                    self.search_buffer[j] = (xx + 1, yy);
-                    j += 1;
-                }
-                if yy > 0 && !self.open[yy - 1][xx] {
-                    self.search_buffer[j] = (xx, yy - 1);
-                    j += 1;
-                }
-                if xx > 0 && !self.open[yy][xx - 1] {
-                    self.search_buffer[j] = (xx - 1, yy);
-                    j += 1;
-                }
-            }
-        }
-
-        self.open[y][x]
-    }
-}
 
 #[macroquad::main("Gloamwood")]
 async fn main() {
@@ -143,7 +54,7 @@ async fn main() {
         .collect();
 
     let dest_size = Some(vec2(S, S));
-    let mut world = WorldMap::new(mapw, maph);
+    let mut world = worldmap::WorldMap::new(mapw, maph);
     world.terrains = terrains;
 
     // ██╗███╗   ██╗██╗████████╗
---
+++ src/worldmap.rs
@@ -0,0 +1,89 @@
+pub struct WorldMap {
+    mapw: usize,
+    maph: usize,
+    pub terrains: Vec<Vec<i16>>,
+    pub monsters: Vec<Vec<i16>>,
+    pub auras: Vec<Vec<i16>>,
+    pub open: Vec<Vec<bool>>,
+    search_buffer: Vec<(usize, usize)>,
+}
+
+impl WorldMap {
+    pub fn new(mapw: usize, maph: usize) -> Self {
+        Self {
+            mapw,
+            maph,
+            terrains: vec![vec![0; mapw]; maph],
+            monsters: vec![vec![0; mapw]; maph],
+            auras: vec![vec![0; mapw]; maph],
+            open: vec![vec![false; mapw]; maph],
+            search_buffer: vec![(0, 0); maph * mapw],
+        }
+    }
+
+    pub fn set_monster(&mut self, x: usize, y: usize, n: i16) {
+        self.monsters[y][x] = n;
+        for yi in 0..3 {
+            let yy = y + yi;
+            if yy == 0 || yy > self.maph {
+                continue;
+            }
+
+            for xi in 0..3 {
+                let xx = x + xi;
+                if xx == 0 || xx > self.mapw {
+                    continue;
+                }
+
+                self.auras[yy - 1][xx - 1] += n;
+            }
+        }
+    }
+
+    pub fn open_tile(&mut self, x: usize, y: usize, val: Option<bool>) -> bool {
+        // clamp x y
+        let x = if x >= self.mapw { self.mapw - 1 } else { x };
+        let y = if y >= self.maph { self.maph - 1 } else { y };
+
+        if let Some(v) = val {
+            let mut j = 0;
+
+            self.search_buffer[j] = (x, y);
+            j += 1;
+
+            while j > 0 {
+                let (xx, yy) = self.search_buffer[j - 1];
+                j -= 1;
+
+                if self.open[yy][xx] {
+                    continue;
+                };
+
+                self.open[yy][xx] = true;
+
+                if self.auras[yy][xx] > 0 {
+                    continue;
+                }
+
+                if yy < self.maph - 1 && !self.open[yy + 1][xx] {
+                    self.search_buffer[j] = (xx, yy + 1);
+                    j += 1;
+                }
+                if xx < self.mapw - 1 && !self.open[yy][xx + 1] {
+                    self.search_buffer[j] = (xx + 1, yy);
+                    j += 1;
+                }
+                if yy > 0 && !self.open[yy - 1][xx] {
+                    self.search_buffer[j] = (xx, yy - 1);
+                    j += 1;
+                }
+                if xx > 0 && !self.open[yy][xx - 1] {
+                    self.search_buffer[j] = (xx - 1, yy);
+                    j += 1;
+                }
+            }
+        }
+
+        self.open[y][x]
+    }
+}
